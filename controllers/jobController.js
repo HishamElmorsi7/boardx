3f19ffd528d14d0c480688488ed2c3131c85e1d8
Hisham <hmelmorsi@gmail.com>
2023-03-28 15:29:01 +0200

    Building the job controller

---
+++ controllers/jobController.js
@@ -0,0 +1,19 @@
+exports.getAllJobs = () => {
+    console.log('Get all jobs');
+}
+
+exports.getJob = () => {
+    console.log('Get a job');
+}
+
+exports.createJob = () => {
+    console.log('Create a job');
+}
+
+exports.updateJob = () => {
+    console.log('update a job');
+}
+
+exports.deleteJob = () => {
+    console.log('delete a job');
+}
