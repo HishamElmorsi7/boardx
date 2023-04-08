f2dd8a741b65b34de9c78ce9324c06dde8ec1ab7
Hisham <hmelmorsi@gmail.com>
2023-04-09 01:20:19 +0200

    Connection with DB

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = require('./app')
 
 dotenv.config({path: './config.env'})
 
+DB = process.env.DATABASE_LOCAL
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {console.log('Connected successfully with DB')})
+
 const port = process.env.PORT || 3000
 app.listen(port, ()=>{
     console.log('Server started on port 8000')
