2d5f21a2daf25145ea8fa6a5f441792889e15f36
Hisham <hmelmorsi@gmail.com>
2023-04-10 22:16:15 +0200

    using Json.parse middleware

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const jobRouter = require('./routes/jobRoutes')
 
 const app = express();
 
+// MIDDLEWARES
+app.use(express.json())
+
+// ROUTES
 app.use('/api/v1/jobs', jobRouter)
 
 module.exports = app
